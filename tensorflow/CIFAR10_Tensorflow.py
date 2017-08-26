6756b47e280df89cdf12489f95183a720b5a42d7
chenwu <chengyi.wu@outlook.com>
2017-08-26 15:37:27 +0800

    +tf.contrib.layers

--- tensorflow/CIFAR10_Tensorflow.py
+++ tensorflow/CIFAR10_Tensorflow.py
@@ -17,7 +17,7 @@ def get_CIFAR10_data(num_training=49000, num_validation=1000, num_test=10000):
     # Load the raw CIFAR-10 data
     system = platform.system()
     if system == "Darwin":
-        cifar10_dir = './cifar-10/cifar-10-batches-py'
+        cifar10_dir = './cifar-10-batches-py'
     elif system == 'Windows':
         cifar10_dir = '.\\cifar-10'
     else:
@@ -85,12 +85,10 @@ def complex_model(X,y,is_training):
     b2 = tf.get_variable("b2", shape=[10])
 
     # define our graph (e.g. two_layer_convnet)
-    a1 = tf.nn.conv2d(X, Wconv1, strides=[1,1,1,1], padding='SAME') + bconv1
+    a1 = tf.nn.conv2d(X, Wconv1, strides=[1,1,1,1], padding='SAME') + bconv1 # 32*32*32
     h1 = tf.nn.relu(a1)
-    
-    # h1 = tf.contrib.layers.batch_norm(h1, center=True, scale=True, is_training=is_training)
-    h1 = tf.nn.max_pool(h1, ksize=[1,2,2,1], strides=[1,2,2,1], padding='VALID')
-    print(h1.shape)
+    h1 = tf.contrib.layers.batch_norm(h1, center=True, scale=True, is_training=is_training)
+    h1 = tf.nn.max_pool(h1, ksize=[1,2,2,1], strides=[1,2,2,1], padding='VALID') # 16*16*32
 
     h1_flat = tf.reshape(h1,[-1,16 * 16 * 32])
     a2 = tf.matmul(h1_flat,W1) + b1
@@ -101,27 +99,98 @@ def complex_model(X,y,is_training):
 
 def vgg_model(X, y, is_training):
     '''
-    (3,3) 64 filters
+    CONV3-64
+    CONV3-128
+    CONV3-256
     '''
     print("Running vgg_model")
-    # 7x7x3 with 32 filters
-    Wconv1 = tf.get_variable("Wconv1", shape=[3, 3, 3, 32])
-    bconv1 = tf.get_variable("bconv1", shape=[32])
+    xavier = tf.contrib.layers.xavier_initializer()
+    # CONV3-64
+    Wconv1 = tf.get_variable(
+        name='Wconv1',
+        shape=(3,3,3,64),
+        initializer=xavier
+    )
+    bconv1 = tf.get_variable(
+        name='bconv1',
+        shape=(64),
+        initializer=tf.zeros_initializer
+    )
+    Wconv2 = tf.get_variable(
+        name='Wconv2',
+        shape=(3,3,64,64),
+        initializer=xavier
+    )
+    bconv2 = tf.get_variable(
+        name='bconv2',
+        shape=(64),
+        initializer=tf.zeros_initializer
+    )
 
-    Wconv2 = tf.get_variable("Wconv2", shape=[3, 3, 32, 32])
-    bconv2 = tf.get_variable("bconv2", shape=[32])
+    # CONV3-128
+    Wconv3 = tf.get_variable(
+        name='Wconv3',
+        shape=(3,3,64,128),
+        initializer=xavier
+    )
+    bconv3 = tf.get_variable(
+        name='bconv3',
+        shape=(128),
+        initializer=tf.zeros_initializer
+    )
+    Wconv4 = tf.get_variable(
+        name='Wconv4',
+        shape=(3,3,128,128),
+        initializer=xavier
+    )
+    bconv4 = tf.get_variable(
+        name='bconv4',
+        shape=(128),
+        initializer=tf.zeros_initializer
+    )
 
-    Wconv3 = tf.get_variable("Wconv3", shape=[3, 3, 32, 64])
-    bconv3 = tf.get_variable("bconv3", shape=[64])
+    # CONV3-256
+    Wconv5 = tf.get_variable(
+        name='Wconv5',
+        shape=(3,3,128,256),
+        initializer=xavier
+    )
+    bconv5 = tf.get_variable(
+        name='bconv5',
+        shape=(256),
+        initializer=tf.zeros_initializer
+    )
+    Wconv6 = tf.get_variable(
+        name='Wconv6',
+        shape=(3,3,256,256),
+        initializer=xavier
+    )
+    bconv6 = tf.get_variable(
+        name='bconv6',
+        shape=(256),
+        initializer=tf.zeros_initializer
+    )
+    Wconv7 = tf.get_variable(
+        name='Wconv7',
+        shape=(3,3,256,256),
+        initializer=xavier
+    )
+    bconv7 = tf.get_variable(
+        name='bconv7',
+        shape=(256),
+        initializer=tf.zeros_initializer
+    )
 
-    Wconv4 = tf.get_variable("Wconv4", shape=[3, 3, 64, 64])
-    bconv4 = tf.get_variable("bconv4", shape=[64])
-
-    W1 = tf.get_variable("W1", shape=[4096, 1024])
-    b1 = tf.get_variable("b1", shape=[1024])
-
-    W2 = tf.get_variable("W2", shape=[1024, 10])
-    b2 = tf.get_variable("b2", shape=[10])
+    W1 = tf.get_variable(
+        name="W1", 
+        shape=[4 * 4 * 256, 10],
+        initializer=xavier
+    )
+    b1 = tf.get_variable(
+        name="b1",
+        shape=[10],
+        initializer=tf.zeros_initializer
+    )
 
     # CONV RELU CONV RELU MAX_POOLING
     a1 = tf.nn.conv2d(X, Wconv1, strides=[1, 1, 1, 1], padding='SAME') + bconv1
@@ -136,14 +205,220 @@ def vgg_model(X, y, is_training):
     h1 = tf.nn.relu(a1)
     h1 = tf.nn.max_pool(h1, ksize=[1,2,2,1], strides=[1,2,2,1], padding='VALID') # 8x8x128
 
-    h1_flat = tf.reshape(h1,[-1,4096])
-    a2 = tf.matmul(h1_flat,W1) + b1
-    h2 = tf.nn.relu(a2)
-
-    y_out = tf.matmul(h2,W2) + b2
+    a1 = tf.nn.conv2d(h1, Wconv5, strides=[1,1,1,1], padding='SAME') + bconv5
+    h1 = tf.nn.relu(a1)
+    a1 = tf.nn.conv2d(h1, Wconv6, strides=[1,1,1,1], padding='SAME') + bconv6
+    h1 = tf.nn.relu(a1)
+    a1 = tf.nn.conv2d(h1, Wconv7, strides=[1,1,1,1], padding='SAME') + bconv7
+    h1 = tf.nn.relu(a1)
+    h1 = tf.nn.max_pool(h1, ksize=[1,2,2,1], strides=[1,2,2,1], padding='VALID') # 4x4x256
+    
+    h1_flat = tf.reshape(h1,[-1, 4 * 4 * 256])
+    y_out = tf.matmul(h1_flat,W1) + b1
     
     return y_out
 
+def vgg_model2(X, y, is_training):
+    '''
+    (3,3) 32 filters
+    '''
+    print("Running vgg_model2")
+    # 7x7x3 with 32 filters
+    Wconv1 = tf.get_variable("Wconv1", shape=[7, 7, 3, 32])
+    bconv1 = tf.get_variable("bconv1", shape=[32])
+
+    Wconv2 = tf.get_variable("Wconv2", shape=[5, 5, 32, 64])
+    bconv2 = tf.get_variable("bconv2", shape=[64])
+
+    Wconv3 = tf.get_variable("Wconv3", shape=[3, 3, 64, 64])
+    bconv3 = tf.get_variable("bconv3", shape=[64])
+
+    Wconv4 = tf.get_variable("Wconv4", shape=[7, 7, 32, 32])
+    bconv4 = tf.get_variable("bconv4", shape=[32])
+
+    Wconv5 = tf.get_variable("Wconv5", shape=[5, 5, 64, 64])
+    bconv5 = tf.get_variable("bconv5", shape=[64])
+
+    Wconv6 = tf.get_variable("Wconv6", shape=[3, 3, 64, 64])
+    bconv6 = tf.get_variable("bconv6", shape=[64])
+
+    W1 = tf.get_variable("W1", shape=[4*4*64,10])
+    b1 = tf.get_variable("b1", shape=[10])
+
+    # CONV RELU CONV RELU MAX_POOLING
+    a1 = tf.nn.conv2d(X, Wconv1, strides=[1, 1, 1, 1], padding='SAME') + bconv1
+    a1 = tf.contrib.layers.batch_norm(a1, center=True, scale=True, is_training=is_training)
+    h1 = tf.nn.relu(a1)
+    a1 = tf.nn.conv2d(h1, Wconv4, strides=[1,1,1,1], padding='SAME') + bconv4
+    a1 = tf.contrib.layers.batch_norm(a1, center=True, scale=True, is_training=is_training)
+    h1 = tf.nn.relu(a1)
+    h1 = tf.nn.max_pool(h1, ksize=[1,2,2,1], strides=[1,2,2,1], padding='VALID') # 16x16x32
+
+    a1 = tf.nn.conv2d(h1, Wconv2, strides=[1,1,1,1], padding='SAME') + bconv2
+    a1 = tf.contrib.layers.batch_norm(a1, center=True, scale=True, is_training=is_training)
+    h1 = tf.nn.relu(a1)
+    a1 = tf.nn.conv2d(h1, Wconv5, strides=[1,1,1,1], padding='SAME') + bconv5
+    a1 = tf.contrib.layers.batch_norm(a1, center=True, scale=True, is_training=is_training)
+    h1 = tf.nn.relu(a1)
+    h1 = tf.nn.max_pool(h1, ksize=[1,2,2,1], strides=[1,2,2,1], padding='VALID') # 8x8x64
+
+    a1 = tf.nn.conv2d(h1, Wconv3, strides=[1,1,1,1], padding='SAME') + bconv3
+    a1 = tf.contrib.layers.batch_norm(a1, center=True, scale=True, is_training=is_training)
+    h1 = tf.nn.relu(a1)
+    a1 = tf.nn.conv2d(h1, Wconv6, strides=[1,1,1,1], padding='SAME') + bconv6
+    a1 = tf.contrib.layers.batch_norm(a1, center=True, scale=True, is_training=is_training)
+    h1 = tf.nn.relu(a1)
+    h1 = tf.nn.max_pool(h1, ksize=[1,2,2,1], strides=[1,2,2,1], padding='VALID') # 4x4x64
+    
+    h1_flat = tf.reshape(h1,[-1,4*4*64])
+    y_out = tf.matmul(h1_flat,W1) + b1
+    
+    return y_out
+
+def vgg_model3(X, y, is_training):
+    '''
+    CONV3-64
+    CONV3-128
+    CONV3-256
+    '''
+    print("Running vgg_model3")
+    xavier = tf.contrib.layers.xavier_initializer()
+
+    W1 = tf.get_variable(
+        name="W1", 
+        shape=[4 * 4 * 256, 10],
+        initializer=xavier
+    )
+    b1 = tf.get_variable(
+        name="b1",
+        shape=[10],
+        initializer=tf.zeros_initializer
+    )
+
+    # CONV RELU CONV RELU MAX_POOLING
+    # CONV3-64
+    a1 = tf.contrib.layers.conv2d(
+        inputs=X,
+        num_outputs=64,
+        kernel_size=3,
+        stride=1,
+        padding='SAME',
+        activation_fn=None
+    )
+    a1 = tf.contrib.layers.batch_norm(
+        inputs=a1,
+        center=True,
+        is_training=is_training
+    )
+    h1 = tf.nn.relu(a1)
+    a1 = tf.contrib.layers.conv2d(
+        inputs=h1,
+        num_outputs=64,
+        kernel_size=3,
+        stride=1,
+        padding='SAME',
+        activation_fn=None
+    )
+    a1 = tf.contrib.layers.batch_norm(
+        inputs=a1,
+        center=True,
+        is_training=is_training
+    )
+    h1 = tf.nn.relu(a1)
+    h1 = tf.contrib.layers.max_pool2d(
+        inputs=h1, 
+        kernel_size=2, 
+        stride=2, 
+        padding='VALID') # 16x16x64
+
+    # CONV3-128
+    a1 = tf.contrib.layers.conv2d(
+        inputs=h1,
+        num_outputs=128,
+        kernel_size=3,
+        stride=1,
+        padding='SAME',
+        activation_fn=None
+    )
+    a1 = tf.contrib.layers.batch_norm(
+        inputs=a1,
+        center=True,
+        is_training=is_training
+    )
+    h1 = tf.nn.relu(a1)
+    a1 = tf.contrib.layers.conv2d(
+        inputs=h1,
+        num_outputs=128,
+        kernel_size=3,
+        stride=1,
+        padding='SAME',
+        activation_fn=None
+    )
+    a1 = tf.contrib.layers.batch_norm(
+        inputs=a1,
+        center=True,
+        is_training=is_training
+    )
+    h1 = tf.nn.relu(a1)
+    h1 = tf.contrib.layers.max_pool2d(
+        inputs=h1, 
+        kernel_size=2, 
+        stride=2, 
+        padding='VALID') # 8x8x128
+
+    # CONV3-256
+    a1 = tf.contrib.layers.conv2d(
+        inputs=h1,
+        num_outputs=256,
+        kernel_size=3,
+        stride=1,
+        padding='SAME',
+        activation_fn=None
+    )
+    a1 = tf.contrib.layers.batch_norm(
+        inputs=a1,
+        center=True,
+        is_training=is_training
+    )
+    h1 = tf.nn.relu(a1)
+    a1 = tf.contrib.layers.conv2d(
+        inputs=h1,
+        num_outputs=256,
+        kernel_size=3,
+        stride=1,
+        padding='SAME',
+        activation_fn=None
+    )
+    a1 = tf.contrib.layers.batch_norm(
+        inputs=a1,
+        center=True,
+        is_training=is_training
+    )
+    h1 = tf.nn.relu(a1)
+    a1 = tf.contrib.layers.conv2d(
+        inputs=h1,
+        num_outputs=256,
+        kernel_size=3,
+        stride=1,
+        padding='SAME',
+        activation_fn=None
+    )
+    a1 = tf.contrib.layers.batch_norm(
+        inputs=a1,
+        center=True,
+        is_training=is_training
+    )
+    h1 = tf.nn.relu(a1)
+    h1 = tf.contrib.layers.max_pool2d(
+        inputs=h1, 
+        kernel_size=2, 
+        stride=2, 
+        padding='VALID') # 4x4x256
+
+    h1_flat = tf.reshape(h1,[-1, 4 * 4 * 256])
+    y_out = tf.matmul(h1_flat,W1) + b1
+    
+    return y_out
 
 def main():
     # Invoke the above function to get our data.
@@ -165,18 +440,23 @@ def main():
     is_training = tf.placeholder(tf.bool)
 
     # y_out = simple_model(X,y)
-    y_out = vgg_model(X,y,is_training=is_training)
+    y_out = vgg_model3(X,y,is_training=is_training)
 
     # define our loss
-    # total_loss = tf.losses.hinge_loss(tf.one_hot(y,10),logits=y_out)
-    total_loss = tf.losses.softmax_cross_entropy(tf.one_hot(y, 10), logits=y_out)
+    total_loss = tf.nn.softmax_cross_entropy_with_logits(
+        labels=tf.one_hot(y, 10), 
+        logits=y_out
+    )
     mean_loss = tf.reduce_mean(total_loss)
 
     # define our optimizer
-    # optimizer = tf.train.AdamOptimizer(5e-4) # select optimizer and set learning rate
-    optimizer = tf.train.RMSPropOptimizer(1e-3)
-    # optimizer = tf.train.GradientDescentOptimizer(1e-4)
-    train_step = optimizer.minimize(mean_loss)
+    optimizer = tf.train.AdamOptimizer(1e-3) # select optimizer and set learning rate
+    # optimizer = tf.train.RMSPropOptimizer(learning_rate=1e-3)
+    # optimizer = tf.train.GradientDescentOptimizer(1e-3)
+
+    extra_update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+    with tf.control_dependencies(extra_update_ops):
+        train_step = optimizer.minimize(mean_loss)
 
     def run_model(session, predict, loss_val, Xd, yd,
               epochs=1, batch_size=64, print_every=100,
@@ -243,10 +523,10 @@ def main():
         return total_loss,total_correct
 
     with tf.Session() as sess:
-        with tf.device("/cpu:0"): #"/cpu:0" or "/gpu:0" 
+        with tf.device("/gpu:0"): #"/cpu:0" or "/gpu:0" 
             sess.run(tf.global_variables_initializer())
             print('Training')
-            run_model(sess,y_out,mean_loss,X_train,y_train,1,64,100,train_step,True)
+            run_model(sess,y_out,mean_loss,X_train,y_train,5,64,100,train_step)
             print('Validation')
             run_model(sess,y_out,mean_loss,X_val,y_val,1,64)
 
